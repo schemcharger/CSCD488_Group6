f25eaae3d0ad022450609acd324485130a66e34a
Neriah Graham <ngraham7@ewu.edu>
2022-11-15 10:57:29 -0800

    Added constructor

--- src/MagicItem.java
+++ src/MagicItem.java
@@ -2,10 +2,21 @@ import java.awt.*;
 
 public class MagicItem {
     enum ItemType{
-        ARMOUR, CATALYST, GENERIC, WEAPON;
+        ARMOUR, CATALYST, GENERIC, WEAPON
     }
     private String name;
     private ItemType type;
     private String description;
     private Color[][] art;
+
+    public MagicItem(String name, ItemType type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public MagicItem(String name, ItemType type, String description) {
+        this.name = name;
+        this.type = type;
+        this.description = description;
+    }
 }
