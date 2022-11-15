cbf2753db20e80d3f22845ec6e2391792359ba9d
Neriah Graham <ngraham7@ewu.edu>
2022-11-15 10:54:41 -0800

    Added fields to MagicItem

--- src/MagicItem.java
+++ src/MagicItem.java
@@ -1,2 +1,11 @@
+import java.awt.*;
+
 public class MagicItem {
+    enum ItemType{
+        ARMOUR, CATALYST, GENERIC, WEAPON;
+    }
+    private String name;
+    private ItemType type;
+    private String description;
+    private Color[][] art;
 }
